8fb4cc72518f699334c15af42a86e7e2c95be4e8
Karim <kialimkhanov@edu.hse.ru>
2023-11-03 10:59:06 +0300

    add innopolis_robot

---
+++ Innopolis/Calibration.js
@@ -0,0 +1,31 @@
+var gyro = brick.gyroscope()
+
+var calibrationDelay = 60000
+
+
+
+gyro.calibrationFinished.connect(function() {
+
+	calibValues = gyro.getCalibrationValues()
+
+	print("Calibration values: " + calibValues)
+
+	
+
+	
+
+	gyro.setCalibrationValues(calibValues)
+
+	
+
+	script.quit()
+
+	
+
+})
+
+
+
+gyro.calibrate(calibrationDelay)
+
+script.run()
